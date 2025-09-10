0f59759dcee4491e403cbc5f760663e82cf68cf7
Janeah <marianajaneramos05@gmail.com>
2025-09-10 20:33:12 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React from "react";
+
+console.log(React.version); // check the react version
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <h1>Heading Text</h1>
     <App />
   </StrictMode>,
 )
+
+
